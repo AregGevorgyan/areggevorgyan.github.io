d4b6c64bb86feef5d3dfeece9724eff4618965d8
AregGevorgyan <109322954+AregGevorgyan@users.noreply.github.com>
2025-08-29 04:05:43 +0000

    Deploying to gh-pages from @ AregGevorgyan/areggevorgyan.github.io@79726105a37462043feaecec743389370426f46e 🚀

---
+++ _app/immutable/chunks/1.3e06c3d0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_error.svelte.1e27624e.js";export{t as component};
---
+++ _app/immutable/entry/_page.svelte.79bd6a90.js
@@ -0,0 +1,17 @@
+import{S as B,i as G,s as H,y as L,a as M,k as l,q as y,z as T,c as $,l as c,m as d,r as _,h as s,n as h,J,A as K,b as R,C as t,F as N,g as O,d as Q,B as W}from"../chunks/index.07d369e4.js";import{S as X}from"../chunks/Seo.498210fd.js";function Y(A){let n,b,r,a,f,k,C,v,E,S,o,D,m,P,x,V,u,p,U,I;return n=new X({props:{title:"Areg Gevorgyan",description:"Researcher, Builder, and Computer Science and Mathematics student."}}),{c(){L(n.$$.fragment),b=M(),r=l("div"),a=l("div"),f=l("p"),k=y(`Hi, I’m an undergraduate student at University of Maryland studying
+      Mathematics and Computer Science. I am currently interning at the
+      Collective Dynamics and Control Lab at UMD, as well as working on personal
+      projects such as building machine learning algorithms from scratch and
+      formalizing math in lean. I previously worked as a VR developer for the
+      Earth Sciences department and led the Math Team at my high school.`),C=M(),v=l("p"),E=y(`I’m interested in everything related to mathematics and computer science,
+      particularly areas that blur the lines between the two and have
+      implications in both theory and practice, such as machine learning and
+      algorithms.`),S=M(),o=l("p"),D=y("I recently started a substack called "),m=l("a"),P=y("Prime Factor"),x=y("."),V=M(),u=l("div"),p=l("img"),this.h()},l(e){T(n.$$.fragment,e),b=$(e),r=c(e,"DIV",{class:!0});var i=d(r);a=c(i,"DIV",{class:!0});var g=d(a);f=c(g,"P",{});var j=d(f);k=_(j,`Hi, I’m an undergraduate student at University of Maryland studying
+      Mathematics and Computer Science. I am currently interning at the
+      Collective Dynamics and Control Lab at UMD, as well as working on personal
+      projects such as building machine learning algorithms from scratch and
+      formalizing math in lean. I previously worked as a VR developer for the
+      Earth Sciences department and led the Math Team at my high school.`),j.forEach(s),C=$(g),v=c(g,"P",{});var q=d(v);E=_(q,`I’m interested in everything related to mathematics and computer science,
+      particularly areas that blur the lines between the two and have
+      implications in both theory and practice, such as machine learning and
+      algorithms.`),q.forEach(s),S=$(g),o=c(g,"P",{});var w=d(o);D=_(w,"I recently started a substack called "),m=c(w,"A",{class:!0,href:!0});var z=d(m);P=_(z,"Prime Factor"),z.forEach(s),x=_(w,"."),w.forEach(s),g.forEach(s),V=$(i),u=c(i,"DIV",{class:!0});var F=d(u);p=c(F,"IMG",{alt:!0,src:!0}),F.forEach(s),i.forEach(s),this.h()},h(){h(m,"class","link"),h(m,"href","https://a11n.substack.com/"),h(a,"class","space-y-5"),h(p,"alt","Picture of Areg"),J(p.src,U="/assets/images/portrait.jpg")||h(p,"src",U),h(u,"class","space-y-5"),h(r,"class","layout-md text-lg space-y-14")},m(e,i){K(n,e,i),R(e,b,i),R(e,r,i),t(r,a),t(a,f),t(f,k),t(a,C),t(a,v),t(v,E),t(a,S),t(a,o),t(o,D),t(o,m),t(m,P),t(o,x),t(r,V),t(r,u),t(u,p),I=!0},p:N,i(e){I||(O(n.$$.fragment,e),I=!0)},o(e){Q(n.$$.fragment,e),I=!1},d(e){W(n,e),e&&s(b),e&&s(r)}}}function Z(A){return[]}class ae extends B{constructor(n){super(),G(this,n,Z,Y,H,{})}}export{ae as default};
